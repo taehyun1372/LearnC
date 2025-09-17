dd8a1f4617c158f17b2e76ef8e40f34779c091a5
Roy Choi <roy.choi@edwardsvacuum.com>
2025-09-17 18:18:41 +0900

    [5_Determin_Tomorrows_date] Improved using functions

---
+++ Practice/16_Scanf/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World \n");
+    int userInput;
+    scanf("%i", &userInput);
+    printf("This is entered : %i", userInput);
+    
+}
--- ProgrammingInC/Chapter10_Pointers/5_Determin_Tomorrows_date.c
+++ ProgrammingInC/Chapter10_Pointers/5_Determin_Tomorrows_date.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+struct date
+{
+    int month;
+    int day;
+    int year;
+};
+
+bool isLeapYear (struct date d);
+int numberOfDays(struct date d);
 
 int main()
 {
-    struct date
-    {
-        int month;
-        int day;
-        int year;
-    };
-
     struct date today, tomorrow;
-    const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
 
     printf("Enther today's date (mm dd yyyy): ");
     
     scanf("%i%i%i", &today.month, &today.day, &today.year);
-    if (today.day != daysPerMonth[today.month - 1]){
+
+    if (today.day != numberOfDays(today)){
         tomorrow.day = today.day + 1;
         tomorrow.month = today.month;
         tomorrow.year = today.year;
@@ -31,5 +36,25 @@ int main()
         tomorrow.year = today.year;
     }
     printf("Tomorrow's date is %i/%i/%.2i.\n", tomorrow.month, tomorrow.day, tomorrow.year % 100);
+}
 
+int numberOfDays(struct date d)
+{
+    int days;
+    const int daysPerMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (isLeapYear(d) == true && d.month == 2)
+        days = 29;
+    else
+        days = daysPerMonth[d.month - 1];
+    return days;
+}
+
+bool isLeapYear(struct date d)
+{
+    bool leapYearFlag;
+    if((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0) 
+        leapYearFlag = true;
+    else 
+        leapYearFlag = false;
+    return leapYearFlag;
 }
