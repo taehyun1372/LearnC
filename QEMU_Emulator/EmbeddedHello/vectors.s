1e1a7ad49e76ebe5f21fb5bf790c52c5eff95c06
Roy Choi <roy.choi@edwardsvacuum.com>
2025-09-30 15:49:39 +0900

    [Deleted binary files] [Added .gitignore file]

--- QEMU_Emulator/EmbeddedHello/vectors.s
+++
@@ -1,9 +0,0 @@
-    .section .isr_vector, "a", %progbits
-    .word  0x20010000      // 초기 스택 포인터 (RAM 끝)
-    .word  reset           // Reset Handler
-
-    .text
-    .global reset
-reset:
-    bl main
-    b .
